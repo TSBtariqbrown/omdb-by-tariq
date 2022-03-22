dfbf6a86e4b9dc11585c54b9804da37d3223c1f8
Tariq Brown <tariqsb@uci.edu>
2022-03-21 18:46:15 -0700

    ongoing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: '',
+      searchedMovie: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({input: event.target.value})
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({searchedMovie: this.state.input, input: ''})
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>The Open Movie Database</h1>
+        <p>Search for a movie:</p>
+        <form>
+          <input value={this.state.input} onChange={this.handleChange}></input>
+          <button onClick={this.handleSubmit}>Search</button>
+        </form>
+        <MovieCard movieTitle={this.state.searchedMovie}/>
+      </div>
+    )
+  }
+}
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>OMdb</h1>
+        <p>Home</p>
+      </div>
+    )
+  }
 }
 
+class MovieCard extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    if (this.props.movieTitle !== '') {
+      let movie = this.props.movieTitle.replace(' ', '-');
+      let movieInfo = new Request('http://www.omdbapi.com/?apikey=9c4122a2&t=Star-Wars'+ movie)
+    }
+    let movieInfo;
+    fetch('http://www.omdbapi.com/?apikey=9c4122a2&t=Star-Wars')
+      .then(response => response.json())
+      .then(function(data) {
+        movieInfo = data.results
+      })
+      let obj;    
+      fetch('http://www.omdbapi.com/?apikey=9c4122a2&t=Star-Wars')
+        .then(res => res.json())
+        .then(data => obj = data)
+      
+    return (
+      <div>
+        <h1>{this.props.movieTitle}</h1>
+        <p>{obj}</p>
+      </div>
+    )
+  }
+}
 export default App;
